2945a084e5017682daa1f5d43482f92c8793c875
Dhruvil Patel <patel2dhruvil202@gmail.com>
2023-11-17 10:57:47 -0500

    remove

--- src/services/Data.js
+++ src/services/Data.js
@@ -31,11 +31,6 @@ export default class Data {
         subdetails:
           "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the",
       },
-      {
-        name: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the",
-        subdetails:
-          "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the",
-      },
     ];
     return items;
   };
